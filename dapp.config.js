b1ab7c4f669df6b51ad68543c813de81c2651981
nkusikevin <nkusikvnhart@gmail.com>
2022-04-23 20:28:11 +0200

    adding metamask wallet connect

--- dapp.config.js
+++ dapp.config.js
@@ -1,3 +1,7 @@
+
+const FORTMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
+const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+
 const config = {
   title: 'BoredApe Dapp',
   description: 'A showcase dapp which is built for you to create your own',
@@ -7,5 +11,51 @@ const config = {
   price: 0.01
 }
 
+const onboardOptions = {
+  dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+  networkId: 4, // Rinkeby
+  darkMode: true,
+  walletSelect: {
+      heading:"Select your wallet to Buy this Ape",
+    wallets: [
+      { walletName: 'metamask', preferred: true },
+      { walletName: 'coinbase', preferred: true },
+      {
+        walletName: 'walletLink',
+        preferred: true,
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'fortmatic',
+        apiKey: FORTMATIC_KEY,
+        preferred: true
+      },
+      { walletName: 'trust', preferred: true, rpcUrl: RPC_URL },
+      { walletName: 'gnosis', preferred: true },
+      { walletName: 'authereum' },
 
-export { config}
+      {
+        walletName: 'ledger',
+        rpcUrl: RPC_URL
+      },
+      {
+        walletName: 'lattice',
+        rpcUrl: RPC_URL,
+        appName: 'BoredApe Dapp'
+      },
+      {
+        walletName: 'keepkey',
+        rpcUrl: RPC_URL
+      }
+    ]
+  },
+  walletCheck: [
+    { checkName: 'derivationPath' },
+    { checkName: 'accounts' },
+    { checkName: 'connect' },
+    { checkName: 'network' }
+  ]
+}
+
+export { config, onboardOptions }
--- pages/mint.js
+++ pages/mint.js
@@ -1,6 +1,55 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { initOnboard } from '../utils/onboards'
+import { config } from '../dapp.config'
 
 function mint() {
+  const [maxSupply, setMaxSupply] = useState(0)
+  const [totalMinted, setTotalMinted] = useState(0)
+  const [maxMintAmount, setMaxMintAmount] = useState(0)
+  const [paused, setPaused] = useState(false)
+  const [isPublicSale, setIsPublicSale] = useState(false)
+  const [isPreSale, setIsPreSale] = useState(false)
+
+  const [status, setStatus] = useState(null)
+  const [mintAmount, setMintAmount] = useState(1)
+  const [isMinting, setIsMinting] = useState(false)
+  const [onboard, setOnboard] = useState(null)
+  const [walletAddress, setWalletAddress] = useState('')
+
+
+     useEffect(() => {
+       const onboardData = initOnboard({
+         address: (address) => setWalletAddress(address ? address : ''),
+         wallet: (wallet) => {
+           if (wallet.provider) {
+             window.localStorage.setItem('selectedWallet', wallet.name)
+           } else {
+             window.localStorage.removeItem('selectedWallet')
+           }
+         }
+       })
+       setOnboard(onboardData)
+     }, [])
+
+ const previouslySelectedWallet =
+   typeof window !== 'undefined' &&
+   window.localStorage.getItem('selectedWallet')
+
+ useEffect(() => {
+   if (previouslySelectedWallet !== null && onboard) {
+     onboard.walletSelect(previouslySelectedWallet)
+   }
+ }, [onboard, previouslySelectedWallet])
+
+  const connectWalletHandler = async () => {
+    const walletSelected = await onboard.walletSelect()
+    if (walletSelected) {
+      await onboard.walletCheck()
+      window.location.reload(true)
+    }
+  }
+
   return (
     <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background ">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
@@ -11,11 +60,11 @@ function mint() {
 
         <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
           <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
-            <h1 className="font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br  from-brand-green to-brand-blue bg-clip-text text-transparent mt-3">
-             
-            </h1>
+            <h1 className="font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br  from-brand-green to-brand-blue bg-clip-text text-transparent mt-3"></h1>
             <h3 className="text-sm text-pink-200 tracking-widest">
-            
+              {walletAddress
+                ? walletAddress.slice(0, 8) + '...' + walletAddress.slice(-4)
+                : ''}
             </h3>
 
             <div className="flex flex-col md:flex-row md:space-x-14 w-full mt-10 md:mt-14">
@@ -23,7 +72,6 @@ function mint() {
                 <div className="font-coiny z-10 absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-brand-purple rounded-md flex items-center justify-center text-white font-semibold">
                   <p>
                     <span className="text-brand-pink"></span> /{' '}
-                   
                   </p>
                 </div>
 
@@ -35,10 +83,7 @@ function mint() {
 
               <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0">
                 <div className="font-coiny flex items-center justify-between w-full">
-                  <button
-                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
-                    
-                  >
+                  <button className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-6 w-6 md:h-8 md:w-8"
@@ -55,14 +100,9 @@ function mint() {
                     </svg>
                   </button>
 
-                  <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-pink text-3xl md:text-4xl">
-                   
-                  </p>
+                  <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-pink text-3xl md:text-4xl"></p>
 
-                  <button
-                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
-                   
-                  >
+                  <button className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-6 w-6 md:h-8 md:w-8"
@@ -81,7 +121,7 @@ function mint() {
                 </div>
 
                 <p className="text-sm text-pink-200 tracking-widest mt-3">
-                  Max Mint Amount: 
+                  Max Mint Amount:
                 </p>
 
                 <div className="border-t border-b py-4 mt-16 w-full">
@@ -99,12 +139,30 @@ function mint() {
                     </div>
                   </div>
                 </div>
-
-                
+                {/* Mint Button && Connect Wallet Button */}
+                {walletAddress ? (
+                  <button
+                    className={` ${
+                      paused || isMinting
+                        ? 'bg-gray-900 cursor-not-allowed'
+                        : 'bg-gradient-to-br from-brand-purple to-brand-pink shadow-lg hover:shadow-pink-400/50'
+                    } font-coiny mt-12 w-full px-6 py-3 rounded-md text-2xl text-white  mx-4 tracking-wide uppercase`}
+                    disabled={paused || isMinting}
+                    // onClick={isPreSale ? presaleMintHandler : publicMintHandler}
+                  >
+                    {isMinting ? 'Minting...' : 'Mint'}
+                  </button>
+                ) : (
+                  <button
+                    className="font-coiny mt-12 w-full bg-gradient-to-br from-brand-purple to-brand-pink shadow-lg px-6 py-3 rounded-md text-2xl text-white hover:shadow-pink-400/50 mx-4 tracking-wide uppercase"
+                    onClick={connectWalletHandler}
+                  >
+                    Connect Wallet
+                  </button>
+                )}
               </div>
             </div>
 
-       
             <div className="border-t border-gray-800 flex flex-col items-center mt-10 py-2 w-full">
               <h3 className="font-coiny text-2xl text-brand-pink uppercase mt-6">
                 Contract Address
