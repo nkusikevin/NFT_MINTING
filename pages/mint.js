2844fc3e5054b57007b48d2fd1cc99240dcc5720
nkusikevin <nkusikvnhart@gmail.com>
2022-04-23 18:08:02 +0200

    adding a minting page

---
+++ pages/mint.js
@@ -0,0 +1,120 @@
+import React from 'react'
+
+function mint() {
+  return (
+    <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background ">
+      <div className="relative w-full h-full flex flex-col items-center justify-center">
+        <img
+          src="/images/blur.jpeg"
+          className="animate-pulse absolute inset-auto block w-full min-h-screen object-cover"
+        />
+
+        <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
+          <div className="z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
+            <h1 className="font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br  from-brand-green to-brand-blue bg-clip-text text-transparent mt-3">
+             
+            </h1>
+            <h3 className="text-sm text-pink-200 tracking-widest">
+            
+            </h3>
+
+            <div className="flex flex-col md:flex-row md:space-x-14 w-full mt-10 md:mt-14">
+              <div className="relative w-full">
+                <div className="font-coiny z-10 absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-brand-purple rounded-md flex items-center justify-center text-white font-semibold">
+                  <p>
+                    <span className="text-brand-pink"></span> /{' '}
+                   
+                  </p>
+                </div>
+
+                <img
+                  src="/images/13.png"
+                  className="object-cover w-full sm:h-[280px] md:w-[250px] rounded-md"
+                />
+              </div>
+
+              <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0">
+                <div className="font-coiny flex items-center justify-between w-full">
+                  <button
+                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                    
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 md:h-8 md:w-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </button>
+
+                  <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-pink text-3xl md:text-4xl">
+                   
+                  </p>
+
+                  <button
+                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                   
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 md:h-8 md:w-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M18 12H6"
+                      />
+                    </svg>
+                  </button>
+                </div>
+
+                <p className="text-sm text-pink-200 tracking-widest mt-3">
+                  Max Mint Amount: 
+                </p>
+
+                <div className="border-t border-b py-4 mt-16 w-full">
+                  <div className="w-full text-xl font-coiny flex items-center justify-between text-brand-yellow">
+                    <p>Total</p>
+
+                    <div className="flex items-center space-x-3">
+                      <p>
+                        {/* {Number.parseFloat(config.price * mintAmount).toFixed(
+                          2
+                        )}{' '} */}
+                        ETH
+                      </p>{' '}
+                      <span className="text-gray-400">+ GAS</span>
+                    </div>
+                  </div>
+                </div>
+
+                
+              </div>
+            </div>
+
+       
+            <div className="border-t border-gray-800 flex flex-col items-center mt-10 py-2 w-full">
+              <h3 className="font-coiny text-2xl text-brand-pink uppercase mt-6">
+                Contract Address
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default mint
